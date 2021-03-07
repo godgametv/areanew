246e6a63fa530ddf1bf24334a70e2359f95a98da
godgametv <80158977+godgametv@users.noreply.github.com>
2021-03-07 14:40:19 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+import subprocess
+subprocess.run("python2 b.py",shell=True,check=True)  
+import subprocess
+subprocess.run("python LOADING.py",shell=True,check=True)
+import subprocess
+subprocess.run("pkg update",shell=True,check=True)
+import subprocess
+subprocess.run("python teks.py",shell=True,check=True)
+
+
